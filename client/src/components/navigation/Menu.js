c9bf03b57e5fa671f20326c8f2d53c98d864b41b
Gilad Tsabar <gilad@tsabar.net>
2020-10-02 17:34:21 +0200

    add missing button import

--- client/src/components/navigation/Menu.js
+++ client/src/components/navigation/Menu.js
@@ -3,6 +3,7 @@ import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
+import Button from '@material-ui/core/Button';
 import MenuIcon from '@material-ui/icons/Menu';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import EditIcon from '@material-ui/icons/Edit';
